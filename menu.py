e2a422e4efc80cf08d3190b9c07ae960ceae55fb
VirginiaFalconi <115593408+VirginiaFalconi@users.noreply.github.com>
2023-06-18 10:13:29 -0300

    menu

---
+++ menu.py
@@ -0,0 +1,44 @@
+import mysql.connector
+
+conexion = mysql.connector.connect(user = 'root', password = '', 
+                                   host = 'localhost',
+                                   database = 'normativa',
+                                   port = '3306')
+
+print(conexion)
+
+
+
+def mostrar_menu():
+    print("---- MENÚ ----")
+    print("1. Agregar Registro: ")
+    print("2. Mostrar Registro: ")
+    print("3. Actualizar Registro: ")
+    print("4. Eliminar Registro: ")
+    print("5. Salir")
+    print("--------------")
+
+while True:
+
+    mostrar_menu()
+    opcion = input("Ingrese la opción deseada: ")
+
+    if opcion == "1":
+        print("Ingrese un nuevo registro")
+
+    elif opcion == "2":
+        print("Mostrar registro")
+       
+
+    elif opcion == "3":
+        print("Actualizar registro")
+       
+    elif opcion == "4":
+        print("Borrar un registro")
+       
+    elif opcion == "5":
+        print("Gracias por usar nuestra base de datos")
+        break
+ 
+    else:
+        print("Seleccione una opcion valida")
